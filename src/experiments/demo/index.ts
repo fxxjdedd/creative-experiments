b634af81aae4ab54b0cd0bb26a191af6036ad0e3
fxxjdedd <fxxjdedd@qq.com>
2024-12-15 19:29:22 +0800

    commit temp spotted shader

--- src/experiments/demo/index.ts
+++ src/experiments/demo/index.ts
@@ -1,8 +1,8 @@
 export const main = /*glsl*/ `
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-    float a = 0.5;
-    float b = 0.5;
+    float a = 0.36;
+    float b = 0.57;
 
     vec2 encodedA = encodeFloatToVec2(a);
     vec2 encodedB = encodeFloatToVec2(b);
@@ -11,7 +11,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     float decodedB = decodeVec2ToFloat(encodedB);
 
     fragColor = vec4(vec3(decodedA, decodedB, 0.0), 1.0);
-    // fragColor = vec4(vec3(0.5, 0.5, 0.0), 1.0);
+    // fragColor = vec4(vec3(a, b, 0.0), 1.0);
 }
 
 `;
--- src/experiments/spotted/index.ts
+++ src/experiments/spotted/index.ts
@@ -10,6 +10,7 @@ const float FEED_U_MAX = 0.04470;
 const float KILL_V_MIN = 0.06516;
 const float KILL_V_MAX = 0.05789;
 
+
 vec4 encode(float U, float V) {
     return vec4(encodeFloatToVec2(U), encodeFloatToVec2(V));
 }
@@ -42,9 +43,11 @@ vec2 computeLaplace(vec2 uv, float U, float V) {
 }
 
 
+
 @swappable 5
-void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    vec2 uv = (coord2uv(fragCoord) + 1.0) / 2.0;
+void mainImage(out vec4 fragColor, in vec2 fragCoord, out vec4 debugColor) {
+    vec2 uv = fragCoord/iResolution.xy;
+    // vec2 uv = (coord2uv(fragCoord) + 1.0) / 2.0;
     vec4 backColor = texture(iBackBuffer, uv);
 
     vec2 UV = decode(backColor);
@@ -55,40 +58,34 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec4 sampledChannel = vec4(0.0, 1.0, 0.0, 0.0);
     float mapValue = dot(sampledChannel, texture2D(iChannel0, uv));
     mapValue = sqrt(clamp(mapValue - 0.1, 0.0, 1.0));
-    // mapValue = texture2D(iChannel0, uv).r;
-
-    // float feedA = mix(0.02220, 0.04470, mapValue);
-    // float killB = mix(0.06516, 0.05789, mapValue);
-    // float diffuseA = 0.210;
-    // float diffuseB = 0.105;
 
     float feedU = mix(FEED_U_MIN, FEED_U_MAX, mapValue);
     float killV = mix(KILL_V_MIN, KILL_V_MAX, mapValue);
 
-
-    float reactionPossibility = U * V * V;
     vec2 laplace = computeLaplace(uv, U, V);
+    float reactionPossibility = U * V * V;
 
-    float nextU = U + RATE_U*laplace.x - reactionPossibility + feedU*(1.0 - U);
-    float nextV = V + RATE_V*laplace.y + reactionPossibility - (killV + feedU)*V;
+    float dt = 0.01;
 
-
+    float nextU = U + dt*(RATE_U*laplace.x - reactionPossibility + feedU*(1.0 - U));
+    float nextV = V + dt*(RATE_V*laplace.y + reactionPossibility - (killV + feedU)*V);
 
     vec4 nextColor = encode(nextU, nextV);
 
     fragColor = nextColor;
-    // fragColor = vec4(vec2(laplace), 1.0, 1.0);
+    // debugColor = vec4(reactionPossibility,0.0, 0.0, 1.0);
 }`;
 
 export const main = /*glsl*/ `
-void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+void mainImage(out vec4 fragColor, in vec2 fragCoord, out vec4 debugColor) {
     vec2 uv = fragCoord/iResolution.xy;
-    vec4 original = texture(iGBuffer0, uv);  // Buffer A的原始结果
-    // vec2 decoded = vec2(decodeVec2ToFloat(original.xy), decodeVec2ToFloat(original.zw));
-    // vec4 circle = vec4(vec2(decoded.x, decoded.y), 0.0, 1.0);
+    vec4 original = texture2D(iGBuffer0, uv);  // Buffer A的原始结果
+    vec2 decoded = vec2(decodeVec2ToFloat(original.xy), decodeVec2ToFloat(original.zw));
+
+    float v = step(0.2, decoded.x);
+    float color = step(0.2, v);
     
-    // 简单混合原始图和模糊图
-    fragColor = vec4(vec3(original.b), 1.0);
+    fragColor = vec4(vec3(color), 1.0);
 }`;
 
 export const shaders = [bufferA, main];
